596969ed8024f1c333145cae4a68f1303e8cd874
Mario <mario@die-davids.de>
2018-12-21 16:01:13 +0100

    initial commit

---
+++ modules/web/src/com/rtcab/cedps/web/screens/OpeningScreen.java
@@ -0,0 +1,36 @@
+package com.rtcab.cedps.web.screens;
+
+import com.haulmont.bali.util.ParamsMap;
+import com.haulmont.cuba.core.entity.KeyValueEntity;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.gui.WindowManager.OpenType;
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.cuba.gui.components.AbstractWindow;
+import java.util.UUID;
+import javax.inject.Inject;
+
+public class OpeningScreen extends AbstractWindow {
+
+
+  @Inject
+  protected Metadata metadata;
+
+  public void openParamsScreen() {
+
+    KeyValueEntity keyValueEntity = createKeyValueEntity();
+
+    AbstractEditor editor = openEditor("dynamic-params-screen", keyValueEntity, OpenType.DIALOG,
+        ParamsMap.of("key", "value"));
+
+    editor.addCloseWithCommitListener(() -> showNotification("Foo: ", keyValueEntity.getValue("foo")));
+  }
+
+  private KeyValueEntity createKeyValueEntity() {
+    KeyValueEntity keyValueEntity = metadata.create(KeyValueEntity.class);
+
+    keyValueEntity.setValue("foo", "bar");
+    keyValueEntity.setIdName("id");
+    keyValueEntity.setId(UUID.randomUUID());
+    return keyValueEntity;
+  }
+}
---
+++ modules/web/src/com/rtcab/cedps/web/screens/params/KeyValueEntityEdit.java
@@ -0,0 +1,7 @@
+package com.rtcab.cedps.web.screens.params;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.cuba.core.entity.KeyValueEntity;
+
+public class KeyValueEntityEdit extends AbstractEditor<KeyValueEntity> {
+}
