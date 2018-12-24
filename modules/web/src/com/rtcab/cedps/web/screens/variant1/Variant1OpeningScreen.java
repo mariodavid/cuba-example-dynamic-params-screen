519fbb68297fa6e73538c5754cfe44d6912aa40e
Konstantin Krivopustov <krivopustov@haulmont.com>
2018-12-24 13:50:10 +0400

    Migrate to framework v7

--- modules/web/src/com/rtcab/cedps/web/screens/variant1/Variant1OpeningScreen.java
+++ modules/web/src/com/rtcab/cedps/web/screens/variant1/Variant1OpeningScreen.java
@@ -1,28 +1,50 @@
 package com.rtcab.cedps.web.screens.variant1;
 
-import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.core.entity.KeyValueEntity;
 import com.haulmont.cuba.core.global.Metadata;
-import com.haulmont.cuba.gui.WindowManager.OpenType;
-import com.haulmont.cuba.gui.components.AbstractEditor;
-import com.haulmont.cuba.gui.components.AbstractWindow;
-import java.util.UUID;
+import com.haulmont.cuba.gui.Notifications;
+import com.haulmont.cuba.gui.ScreenBuilders;
+import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.Subscribe;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+import com.rtcab.cedps.web.screens.variant1.params.Variant1DynamicParamsScreen;
+
 import javax.inject.Inject;
+import java.util.UUID;
 
-public class Variant1OpeningScreen extends AbstractWindow {
-
+@UiController("variant1-opening-screen")
+@UiDescriptor("variant1-opening-screen.xml")
+public class Variant1OpeningScreen extends Screen {
 
   @Inject
   protected Metadata metadata;
 
-  public void openParamsScreen() {
+  @Inject
+  private ScreenBuilders screenBuilders;
+  @Inject
+  private Notifications notifications;
 
+  @Subscribe("openBtn")
+  public void openParamsScreen(Button.ClickEvent clickEvent) {
     KeyValueEntity keyValueEntity = createKeyValueEntity();
 
-    AbstractEditor editor = openEditor("variant1-dynamic-params-screen", keyValueEntity, OpenType.DIALOG,
-        ParamsMap.of("key", "value"));
+    Variant1DynamicParamsScreen editor = screenBuilders.editor(KeyValueEntity.class, this)
+            .withScreenClass(Variant1DynamicParamsScreen.class)
+            .editEntity(keyValueEntity)
+            .build();
+    editor.addAfterCloseListener(afterCloseEvent -> {
+      if (afterCloseEvent.getCloseAction().equals(WINDOW_COMMIT_AND_CLOSE_ACTION)) {
+        notifications.create().withCaption(editor.getEditedEntity().getValue("foo")).show();
+      }
+    });
+    editor.show();
 
-    editor.addCloseWithCommitListener(() -> showNotification("Foo: " + keyValueEntity.getValue("foo")));
+//    AbstractEditor editor = openEditor("variant1-dynamic-params-screen", keyValueEntity, OpenType.DIALOG,
+//        ParamsMap.of("key", "value"));
+//
+//    editor.addCloseWithCommitListener(() -> showNotification("Foo: " + keyValueEntity.getValue("foo")));
   }
 
   private KeyValueEntity createKeyValueEntity() {
